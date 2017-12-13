cd587f0fffa67fded226a132e82ba1d461d601f3
puneetpalkaur <billingpuneet.13@gmail.com>
2017-12-13 03:34:35 -0500

    added code to fetch webcam data for analytics

--- partial/analytics-partial/analytics-partial.js
+++ partial/analytics-partial/analytics-partial.js
@@ -1,19 +1,48 @@
-angular.module('shield').controller('AnalyticsPartialCtrl',function($scope){
+angular.module('shield').controller('AnalyticsPartialCtrl',function($scope,shieldService){
 
-$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-//  $scope.series = ['Webcam Images', 'Screenshots'];
-//  $scope.data = [
-//    [65, 59, 80, 81, 56, 55, 40],
-//    [28, 48, 40, 19, 86, 27, 90]
-//  ];
+var date_time_list = [];
+var year = ["2017"];
+var webData = '';
+var labels = [];
+var myMap = new Map();
+myMap.set("Aug",   1);
+myMap.set("Nov", 2);
+var dict = {"Jan":0, "Feb":0, "Mar":0, "Apr":0, "May":0, "Jun":0, "Jul":0,"Aug":0,"Sep":0,"Oct":0,"Nov":0,"Dec":0};
+var completeD = {"":dict};
+shieldService.getWebCamData().then(function (data) {
+
+        $scope.webCamData = data.message;
+        webData =  $scope.webCamData ;
+        for(i=0;i<webData.length;i++)
+        {
+            date_time_list.push(webData[i].webcam_date_time)
+        }
+
+        for(k=0;k<date_time_list.length;k++)
+        {
+            var temp = date_time_list[k];
+            var yearselected = temp.slice(12,17);
+            var monthselected = temp.slice(8,11);
+            dict[monthselected] = dict[monthselected]+1;
+        /*    completeD.yearselected = dict;*/
+          }
+console.log("complete dict printed below");
+console.log(completeD);
+$scope.year = ["2017", "2016", "2015"];
+
+$scope.getValue = function (x) {
+                var selYear = $scope.selectedName;
+                console.log("year selected"+selYear)
+              };
+
+
+$scope.labels = Object.keys(dict);
 $scope.series = ['Webcam Images'];
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40]
-  ];
+  $scope.data = [ Object.values(dict)];
   $scope.onClick = function (points, evt) {
     //console.log(points, evt);
   };
-  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }];
   $scope.options = {
     scales: {
       yAxes: [
@@ -22,14 +51,12 @@ $scope.series = ['Webcam Images'];
           type: 'linear',
           display: true,
           position: 'left'
-        },
-        {
-          id: 'y-axis-2',
-          type: 'linear',
-          display: true,
-          position: 'right'
         }
+
       ]
     }
   };
+
+   });
+
 });
