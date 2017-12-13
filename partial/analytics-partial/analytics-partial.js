5e895efa33eaaeaad2af7e748b32b57a783d8b39
puneetpalkaur <billingpuneet.13@gmail.com>
2017-12-13 16:03:00 -0500

    live data integrated into chart1 from webcam table

--- partial/analytics-partial/analytics-partial.js
+++ partial/analytics-partial/analytics-partial.js
@@ -4,41 +4,53 @@ var date_time_list = [];
 var year = ["2017"];
 var webData = '';
 var labels = [];
-var myMap = new Map();
-myMap.set("Aug",   1);
-myMap.set("Nov", 2);
+
 var dict = {"Jan":0, "Feb":0, "Mar":0, "Apr":0, "May":0, "Jun":0, "Jul":0,"Aug":0,"Sep":0,"Oct":0,"Nov":0,"Dec":0};
-var completeD = {"":dict};
+var completeD = {};
 shieldService.getWebCamData().then(function (data) {
 
         $scope.webCamData = data.message;
         webData =  $scope.webCamData ;
-        for(i=0;i<webData.length;i++)
+       for(k=0;k<webData.length;k++)
         {
-            date_time_list.push(webData[i].webcam_date_time)
-        }
-
-        for(k=0;k<date_time_list.length;k++)
-        {
-            var temp = date_time_list[k];
-            var yearselected = temp.slice(12,17);
-            var monthselected = temp.slice(8,11);
-            dict[monthselected] = dict[monthselected]+1;
-        /*    completeD.yearselected = dict;*/
+            var temp = webData[k].webcam_date_time;
+            var yearselected = temp.slice(12,16);
+            completeD[yearselected] = {"Jan":0, "Feb":0, "Mar":0, "Apr":0, "May":0, "Jun":0, "Jul":0,"Aug":0,"Sep":0,"Oct":0,"Nov":0,"Dec":0};
           }
-console.log("complete dict printed below");
-console.log(completeD);
-$scope.year = ["2017", "2016", "2015"];
+        console.log("complete dict printed beforee---");
+        console.log(completeD);
+        for(q=0;q<webData.length;q++)
+        {
+            console.log("i am inside web");
+            var diction = [];
+            var temp = webData[q].webcam_date_time;
+
+            var yearselected = temp.slice(12,16);
+            var monthselected = temp.slice(8,11);
+
+            completeD[yearselected][monthselected] = completeD[yearselected][monthselected]+1;
+
+          }
+
+$scope.year = Object.keys(completeD);
+// set default value in drop down to the current year
+var cal = new Date().getFullYear();
+console.log("cal",cal);
+$scope.selectedName = String(cal);
+setData(String(cal));
 
 $scope.getValue = function (x) {
                 var selYear = $scope.selectedName;
                 console.log("year selected"+selYear)
+                setData(selYear);
               };
 
 
-$scope.labels = Object.keys(dict);
+function setData(selYear)
+{
+$scope.labels =  Object.keys(completeD[selYear]);
 $scope.series = ['Webcam Images'];
-  $scope.data = [ Object.values(dict)];
+  $scope.data = [ Object.values(completeD[selYear])];
   $scope.onClick = function (points, evt) {
     //console.log(points, evt);
   };
@@ -56,6 +68,9 @@ $scope.series = ['Webcam Images'];
       ]
     }
   };
+
+};
+
 
    });
 
